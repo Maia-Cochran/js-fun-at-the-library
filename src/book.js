340f2b468392bb327a295f3bc27738b81e5640d4
Maia Cochran <maia.cochran1@gmail.com>
2022-05-14 20:37:48 -0700

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,12 +1,44 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+return {
+  name: name,
+  age: age,
+  pronouns: pronouns,
+}
+};
+
+function saveReview(review, reviews) {
+ if (reviews.includes(review)) {
+ } else {
+  return reviews.push(review);
+}
+};
+
+function calculatePageCount(book) {
+  return book.length * 20
+};
+
+function writeBook(title, character, genre) {
+  return {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+}
+};
+
+function editBook(book){
+  return book.pageCount = book.pageCount * .75
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,46 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3)
+  shelf.unshift(book);
+};
+
+function unshelfBook(bookTitle, shelf) {
+ for (var i = 0; i < shelf.length; i++) {
+   if (bookTitle === shelf[i].title) {
+      shelf.splice(i, 1);
+  }
+ }
+};
+
+//when the book === the current shelf[i].name, remove the book
+//we have the book title which is a string
+// that is stored in a key value pair in book object
+// the book object is stored in the shelf array
+//if we find the index position of the book object,
+// we can remove it from the array
+
+function listTitles(shelf) {
+  var titles = []
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title)
+  }
+  return titles.join(", ")
+};
+
+function searchShelf(shelf, title) {
+  var result
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      result = true
+    } else {
+      result = false
+    }
+  }
+return result
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
